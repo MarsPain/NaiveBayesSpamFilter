b327282d79fb9028fe4048a630dcf78e692cd309
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2016-03-18 21:03:41 +0800

    have some bugs!

--- OnlyNavieBayes/test.py
+++ OnlyNavieBayes/test.py
@@ -6,12 +6,23 @@
 """
 
 import numpy as np
-import OnlyNavieBayes.NaiveBayes as util
+import OnlyNavieBayes.NavieBayesian as naiveBayes
 
+# 加载训练获取的语料库信息
+vocabularyList = naiveBayes.getVocabularyList('vocabularyList.txt')
 pWordsHealthy = np.loadtxt('pWordsHealthy.txt', delimiter='\t')
 pWordsSpamicity = np.loadtxt('pWordsSpamicity.txt', delimiter='\t')
-print 'pWordsHealthy:', len(pWordsHealthy), np.shape(pWordsHealthy)
-print 'pWordsSpamicity:', len(pWordsSpamicity)
-pSpam = 0.13401507
-pSWi = util.bayesTheoremCalcPSWi(pWordsSpamicity, pWordsHealthy, pSpam)
-print pSWi
+fr = open('pSpam.txt')
+pSpam = float(fr.readline().strip())
+print 'pSpam:', type(pSpam), pSpam
+
+# 保存！！
+pSWi, pHWi = naiveBayes.bayesTheoremCalc(pWordsSpamicity, pWordsHealthy, pSpam)
+
+# 加载测试数据
+filename = '../emails/test/test.txt'
+smsWords, classLables = naiveBayes.loadSMSData(filename)
+testWordsCount = naiveBayes.setOfWordsToVecTor(vocabularyList, smsWords[0])
+testWordsCountArray = np.array(testWordsCount)
+result = naiveBayes.classify(pSWi, pHWi, pSpam, testWordsCountArray)
+print 'result:', result
--- OnlyNavieBayes/training.py
+++ OnlyNavieBayes/training.py
@@ -5,29 +5,33 @@
 @Author: MarkLiu
 """
 import numpy as np
-import OnlyNavieBayes.NaiveBayes as util
+import OnlyNavieBayes.NavieBayesian as naiveBayes
 
 filename = '../emails/training/SMSCollection.txt'
-smsWords, classLables = util.loadSMSData(filename)
-vocabularyList = util.createVocabularyList(smsWords)
+smsWords, classLables = naiveBayes.loadSMSData(filename)
+vocabularyList = naiveBayes.createVocabularyList(smsWords)
 print "生成语料库！"
 trainMarkedWords = []
 for words in smsWords:
-    vocabMarked = util.setOfWordsToVecTor(vocabularyList, words)
+    vocabMarked = naiveBayes.setOfWordsToVecTor(vocabularyList, words)
     trainMarkedWords.append(vocabMarked)
 print "数据标记完成！"
 # 转成array向量
 trainMarkedWords = np.array(trainMarkedWords)
 print "数据转成矩阵！"
-pWordsSpamicity, pWordsHealthy, pSpam = util.trainingNaiveBayes(trainMarkedWords, classLables)
+pWordsSpamicity, pWordsHealthy, pSpam = naiveBayes.trainingNaiveBayes(trainMarkedWords, classLables)
 print 'pSpam:', pSpam
-
+fpSpam = open('pSpam.txt', 'w')
+spam = pSpam.__str__()
+fpSpam.write(spam)
+fpSpam.close()
 # 保存训练生成的语料库信息
 # 保存语料库词汇
 fw = open('vocabularyList.txt', 'w')
 for i in range(len(vocabularyList)):
     fw.write(vocabularyList[i] + '\t')
-
-# 保存pWordsSpamicity和pWordsHealthy
+fw.flush()
+fw.close()
+# 保存训练阶段获取的参数：pWordsSpamicity和pWordsHealthy
 np.savetxt('pWordsSpamicity.txt', pWordsSpamicity, delimiter='\t')
 np.savetxt('pWordsHealthy.txt', pWordsHealthy, delimiter='\t')
