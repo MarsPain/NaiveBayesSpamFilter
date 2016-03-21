fb927ab7bad2ea20f63d88d487d3a58d45b936b9
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2016-03-21 20:01:46 +0800

    have some bugs!

--- OnlyNavieBayes/test.py
+++ OnlyNavieBayes/test.py
@@ -21,18 +21,13 @@ smsWords, classLables = naiveBayes.loadSMSData(filename)
 testWordsCount = naiveBayes.setOfWordsToVecTor(vocabularyList, smsWords[0])
 testWordsMarkedArray = np.array(testWordsCount)
 
-# # 保存！！
-# pSWi = naiveBayes.bayesTheoremCalcPSWi(pWordsSpamicity, pWordsHealthy, pSpam)
-#
-# sorted_pSWi_N, sortedWordsMarked_N = \
-#     naiveBayes.getPreN_pSWi(pSWi, testWordsMarkedArray, N=15)
-#
-# pSWi = naiveBayes.dealWithRareWords(sorted_pSWi_N, pSpam, sortedWordsMarked_N)
-# print 'dealWithRareWords:'
-# print pSWi
-#
-# print np.sum(sorted_pSWi_N * sortedWordsMarked_N)
-# naiveBayes.classify(pSWi)
+# 保存！！
+pSWi = naiveBayes.bayesTheoremCalcPSWi(pWordsSpamicity, pWordsHealthy, pSpam)
+
+sorted_pSWi_N, sortedWordsMarked_N = \
+    naiveBayes.getPreN_pSWi(pSWi, testWordsMarkedArray, N=15)
+
+pSWi = naiveBayes.dealWithRareWords(sorted_pSWi_N, pSpam, sortedWordsMarked_N)
 
 # 计算P(Ci|W)，W为向量。P(Ci|W)只需计算P(W|Ci)P(Ci)
 p1 = sum(testWordsMarkedArray * pWordsSpamicity) + np.log(pSpam)
